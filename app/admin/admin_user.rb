25ce38fe88b3a39bd14a534508d1737a4f9b3927
Mandy Yeung <mandycodes@gmail.com>
2014-09-24 21:02:16 -0400

    Add names to users

--- app/admin/admin_user.rb
+++ app/admin/admin_user.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register AdminUser do
-  permit_params :email, :password, :password_confirmation
+  permit_params :first_name, :last_name, :email, :password, :password_confirmation
 
   index do
     selectable_column
@@ -11,6 +11,8 @@ ActiveAdmin.register AdminUser do
     actions
   end
 
+  filter :first_name
+  filter :last_name
   filter :email
   filter :current_sign_in_at
   filter :sign_in_count
@@ -18,6 +20,8 @@ ActiveAdmin.register AdminUser do
 
   form do |f|
     f.inputs "Admin Details" do
+      f.input :first_name
+      f.input :last_name
       f.input :email
       f.input :password
       f.input :password_confirmation
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140925000342) do
+ActiveRecord::Schema.define(version: 20140925005921) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,8 @@ ActiveRecord::Schema.define(version: 20140925000342) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "admin",                  default: false
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true, using: :btree
